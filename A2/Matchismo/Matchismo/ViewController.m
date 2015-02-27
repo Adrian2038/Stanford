8ef36b47c792db4aff610a15170215094b689afd
Adrian2038 <goglezhu11@gmail.com>
2015-02-27 16:46:39 +0800

    完成Demo Matchismo
    遇到了一个bug
    【NSUnknownKeyException', reason: '[<ViewController 0x7be99780>
    setValue:forUndefinedKey:]】
    不过还好从网上找到了解决方案
    原因为 ： view controller的outlet删除了，但是storyboard的链接没有移除，移调storyboard的链接即可。

--- A2/Matchismo/Matchismo/ViewController.m
+++ A2/Matchismo/Matchismo/ViewController.m
@@ -22,7 +22,7 @@
 - (CardMatchingGame *)game
 {
     if (!_game) {
-        _game = [[CardMatchingGame alloc] initWithCardNumber:self.cardButtons.count
+        _game = [[CardMatchingGame alloc] initWithCardNumber:[self.cardButtons count]
                                                     withDeck:[self createDeck]];
     }
     return _game;
@@ -42,9 +42,26 @@
 
 - (void)updateUI
 {
-    
+    for (UIButton *cardButton in self.cardButtons) {
+        int cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+        
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score : %d", self.game.score];
+    }
 }
 
+- (UIImage *)backgroundImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"cardfront" : @"cardback"];
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return card.isChosen ? card.contents : @"";
+}
 
 
 
